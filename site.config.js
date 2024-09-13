bf706125aa4972fd9ed2cb28ea88788eee3af952
XueYang <894689660@qq.com>
2024-09-13 11:23:42 +0800

    fix: 更新配置

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   profile: {
     name: "风之旅人",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "software developer",
     bio: "探索代码，徜徉生活",
     email: "xueyang20081992@163.com",
     linkedin: "",
@@ -26,7 +26,7 @@ const CONFIG = {
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "zh-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
